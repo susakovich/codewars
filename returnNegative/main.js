1970dfeb6c1b4b55a7ad62c84d184e8711434c21
Danijel <susakovich@gmail.com>
2022-12-13 11:22:36 +0100

    Finish 8kyu exercise

---
+++ returnNegative/main.js
@@ -0,0 +1,20 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+
+const returnNegative = (num) => {
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
+};
+
+console.log(returnNegative(1));
+console.log(returnNegative(-5));
+console.log(returnNegative(0));
+console.log(returnNegative(0.12));
