b7605884a70bc7f463d3168c59bdf3989252e449
Danijel <susakovich@gmail.com>
2022-12-14 10:54:33 +0100

    Finish 8kyu exercise

---
+++ boolYesOrNo/main.js
@@ -0,0 +1,10 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+const boolYesOrNo = (bool) => (bool ? "Yes" : "No");
+
+console.log(boolYesOrNo("Dado"));
+console.log(boolYesOrNo(true));
+console.log(boolYesOrNo(54));
+console.log(boolYesOrNo(0));
+console.log(boolYesOrNo(-23));
+console.log(boolYesOrNo(false));
