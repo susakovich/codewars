122888fd56fc072e5b6c50bfe20ae3cb5b592e67
Danijel <susakovich@gmail.com>
2022-12-24 22:25:00 +0100

    Finish 8kyu kata

---
+++ rmStrSpaces/main.js
@@ -0,0 +1,7 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function rmStringSpaces(str) {
+  return str.split(" ").join("");
+}
+
+console.log(rmStringSpaces("Dad dooooooooo"));
