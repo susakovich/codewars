45d9656f318ed9ca8c42b99a7bc067acec8a1cfd
Danijel <susakovich@gmail.com>
2022-12-25 10:30:25 +0100

    Refactor old if/else to ternary

--- evenOrOdd/main.js
+++ evenOrOdd/main.js
@@ -1,15 +1,19 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-function evenOrOdd(number) {
-  if (number % 2 === 0) {
-    return `Your ${number} is Even`;
-  } else {
-    return `Your ${number} is Odd`;
-  }
-}
+// Old way
+// function evenOrOdd(number) {
+//   if (number % 2 === 0) {
+//     return `Your ${number} is Even`;
+//   } else {
+//     return `Your ${number} is Odd`;
+//   }
+// }
 
-console.log(evenOrOdd(10));
-console.log(evenOrOdd(103));
-console.log(evenOrOdd(51));
-console.log(evenOrOdd(90));
-console.log(evenOrOdd(1232));
+// Ternary operator
+const evenOdd = (num) =>
+  num % 2 === 0 ? `Your ${num} is even` : `Your ${num} is odd`;
+
+console.log(evenOdd(20));
+console.log(evenOdd(13));
+console.log(evenOdd(28));
+console.log(evenOdd(21));
