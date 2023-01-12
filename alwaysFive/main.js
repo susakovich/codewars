7769329d92c2213bcb7485c9ffcb5306219b26a5
Danijel <susakovich@gmail.com>
2023-01-12 21:57:36 +0100

    Finish 8kyu kata

---
+++ alwaysFive/main.js
@@ -0,0 +1,12 @@
+// TODO
+/* Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/ 
+*/
+
+function unusualFive() {
+  return "susak".length;
+}
+
+console.log(unusualFive("hello"));
+console.log(unusualFive(90));
