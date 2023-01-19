ebcaf4083915af385af9027a8cebf2d2215380db
Danijel <susakovich@gmail.com>
2023-01-19 23:38:12 +0100

    Finish 8kyu kata

---
+++ 8kyu/schoolPaperwork/main.js
@@ -0,0 +1,14 @@
+// TODO
+/* 
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+Example:
+n= 5, m=5: 25
+n=-5, m=5:  0
+*/
+
+const paperWork = (n, m) => (n < 0 || m < 0 ? 0 : n * m);
+console.log(paperWork(5, 5));
+console.log(paperWork(-5, 5));
