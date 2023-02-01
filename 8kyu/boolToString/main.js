11dbabff07df91257e8c51c4851a8ab9619f79d0
Danijel <susakovich@gmail.com>
2023-02-01 22:43:05 +0100

    Finish 8kyu kata

---
+++ 8kyu/boolToString/main.js
@@ -0,0 +1,10 @@
+// TODO
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+const boolToStr = (bool) => bool.toString();
+console.log(typeof boolToStr(true));
+console.log(typeof boolToStr(false));
