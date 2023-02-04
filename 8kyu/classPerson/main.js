e8bbf026c03b5a37f44aa4a51580468bd97a8c1a
Danijel <susakovich@gmail.com>
2023-02-04 23:22:43 +0100

    Finish 8kyu kata

---
+++ 8kyu/classPerson/main.js
@@ -0,0 +1,36 @@
+// TODO
+/* 
+Fun with ES6 Classes #1 - People, people, people
+Time for some OOP fun!
+
+Define a class Person with the following properties:
+
+A constructor that accepts 4 arguments: firstName/FirstName (defaults to "John" if not set), lastName/LastName (defaults to "Doe" if not set), age/Age (defaults to 0 if not set) and gender/Gender (defaults to "Male" if not set). These should be stored in this.firstName/this.FirstName, this.lastName/this.LastName, this.age/this.Age and this.gender/this.Gender respectively.
+A method sayFullName/SayFullName that accepts no arguments and returns the full name (e.g. "John Doe")
+A class/static method greetExtraTerrestrials/GreetExtraTerrestrials that accepts one parameter raceName and returns "Welcome to Planet Earth raceName". For example, if the race name is "Martians", it should say "Welcome to Planet Earth Martians"
+You may use any valid syntax you like; however, it is highly recommended that you complete this Kata using ES6 syntax and features.
+*/
+
+class Person {
+  constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+    // shorter way would be
+    Object.assign(this, { firstName, lastName, age, gender });
+    // this.firstName = firstName;
+    // this.lastName = lastName;
+    // this.age = age;
+    // this.gender = gender;
+
+    this.sayFullName = function () {
+      return `${this.firstName} ${this.lastName}`;
+    };
+    this.greetExtraTerrestrials = function (raceName) {
+      this.raceName = raceName;
+      return `Welcome to Planet ${this.raceName}`;
+    };
+  }
+}
+
+let newBoi = new Person("Dado", "Suki", 32, "Male");
+console.log(newBoi.greetExtraTerrestrials("Earth"));
+console.log(newBoi.sayFullName());
+console.log(newBoi.age);
