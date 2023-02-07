35b903e62b8da33c27352ac8a679b8d8a7a2a1cd
Danijel <susakovich@gmail.com>
2023-02-07 22:21:04 +0100

    Finish 8kyu kata

---
+++ 8kyu/cockroach/main.js
@@ -0,0 +1,12 @@
+// TODO
+/*
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+*/
+
+const inSec = (sec) => Math.floor(sec / 0.036);
+console.log(inSec(1.08));
