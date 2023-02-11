f0b70522d5a13160e7712f463b93416101ec0811
Danijel <susakovich@gmail.com>
2023-02-11 22:27:18 +0100

    Finish 7kyu kata

---
+++ 7kyu/sumOfOddNums/main.js
@@ -0,0 +1,21 @@
+// TODO
+/* 
+DESCRIPTION:
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+*/
+
+const sumOfOddNums = (num) => Math.pow(num, 3);
+console.log(sumOfOddNums(2));
+console.log(sumOfOddNums(3));
+console.log(sumOfOddNums(5));
