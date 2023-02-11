cd383317275b7ae56a5693e50ff94fc3f5811eb4
Danijel <susakovich@gmail.com>
2023-02-11 22:24:21 +0100

    Finish 7kyu kata

---
+++ 7kyu/oddOrEven/main.js
@@ -0,0 +1,25 @@
+// TODO
+/*
+Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+*/
+
+function oddOrEven(arr) {
+  return arr.reduce((accu, curr) => accu + curr, 0) % 2 === 0 ? "even" : "odd";
+}
+console.log(oddOrEven([0, 1, 4]));
+console.log(oddOrEven([0, -1, -5]));
