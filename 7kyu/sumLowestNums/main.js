db4c5b3c75ee4ca391076878d061a6d2b39adf26
Danijel <susakovich@gmail.com>
2023-02-15 22:10:00 +0100

    Finish 7kyu kata

---
+++ 7kyu/sumLowestNums/main.js
@@ -0,0 +1,26 @@
+// TODO
+/* 
+DESCRIPTION:
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+*/
+
+const sumLowestIntegers = (array) => {
+  let [a, b] = array.sort((a, b) => a - b);
+  return a + b;
+};
+console.log(sumLowestIntegers([19, 5, 42, 2, 77]));
+console.log(sumLowestIntegers([10, 343445353, 3453445, 3453545353453]));
+
+// another solution
+function sumTwoSmallestNumbers(numbers) {
+  numbers = numbers.sort(function (a, b) {
+    return a - b;
+  });
+  return numbers[0] + numbers[1];
+}
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
+console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]));
