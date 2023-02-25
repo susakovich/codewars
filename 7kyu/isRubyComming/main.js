1ff7b95141c4fdd472c3d51842bb696bfcf14860
Danijel <susakovich@gmail.com>
2023-02-25 21:17:21 +0100

    Finish 7kyu kata - PREP

---
+++ 7kyu/isRubyComming/main.js
@@ -0,0 +1,55 @@
+// TODO
+/*
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+];
+your function should return true.
+*/
+
+// PREP - Parameters, Result, Examples, PseudoCode
+// P: array of objects, for devs that signed up for coding meetup, true if atleast one ruby dev singed up, false if not
+// R: list1, should return true
+// E: list1
+// P: make a function, find ruby developer, ? true : false
+
+let list1 = [
+  {
+    firstName: "Emma",
+    lastName: "Z.",
+    country: "Netherlands",
+    continent: "Europe",
+    age: 29,
+    language: "Ruby",
+  },
+  {
+    firstName: "Piotr",
+    lastName: "B.",
+    country: "Poland",
+    continent: "Europe",
+    age: 128,
+    language: "Javascript",
+  },
+  {
+    firstName: "Jayden",
+    lastName: "P.",
+    country: "Jamaica",
+    continent: "Americas",
+    age: 42,
+    language: "JavaScript",
+  },
+];
+
+function isRubyComing(list) {
+  return list.some((dev) => (dev.language === "Ruby" ? true : false));
+}
+console.log(isRubyComing(list1));
