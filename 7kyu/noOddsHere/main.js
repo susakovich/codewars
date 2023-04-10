4d0d15d117a3177c5a561521e4957b4f19306f40
Danijel <susakovich@gmail.com>
2023-04-10 20:48:27 +0200

    Finish 7kyu kata

---
+++ 7kyu/noOddsHere/main.js
@@ -0,0 +1,8 @@
+// TODO
+/*
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+*/
+
+const noOdds = (values) => values.filter((x) => x % 2 === 0);
