b6085f77e76ad5bf2d06c415cc96f912bef10bc1
Danijel <susakovich@gmail.com>
2023-04-11 12:52:46 +0200

    Finish 8kyu kata

---
+++ 8kyu/countSheep/main.js
@@ -0,0 +1,14 @@
+// TODO
+/*
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+*/
+
+const countSheep = (num) => {
+  return [...Array(num).keys()].map((i) => `${i + 1} sheep...`).join("");
+};
+
+console.log(countSheep(5));
