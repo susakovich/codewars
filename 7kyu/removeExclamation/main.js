90a0a0ab4766b468b414ba41cc924d4afb06453b
Danijel <susakovich@gmail.com>
2023-04-15 12:06:54 +0200

    Finish 7kyu kata

---
+++ 7kyu/removeExclamation/main.js
@@ -0,0 +1,22 @@
+// TODO
+/*
+Description:
+Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
+
+Examples
+remove("Hi!") === ""
+remove("Hi! Hi!") === ""
+remove("Hi! Hi! Hi!") === ""
+remove("Hi Hi! Hi!") === "Hi"
+remove("Hi! !Hi Hi!") === ""
+remove("Hi! Hi!! Hi!") === "Hi!!"
+remove("Hi! !Hi! Hi!") === "!Hi!"
+*/
+
+function remove(string) {
+  return string
+    .split(" ")
+    .filter((i) => i.split("!").length != 2)
+    .join(" ");
+}
+console.log(remove("Hi! Hi!! Hi!"));
