c2954357d192dd1454f6c6ef402a1d53fb95b625
Danijel <susakovich@gmail.com>
2023-06-16 19:23:05 +0200

    Finish 8kyu kata

---
+++ 8kyu/powerOfTwo/main.js
@@ -0,0 +1,13 @@
+// TODO
+/*
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+*/
+function powersOfTwo(n) {
+  let result = [];
+
+  for (let i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+
+  return result;
+}
