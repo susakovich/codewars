c1dc71ccb42b3b3addcbc3614e0ef23c67a2704a
Danijel <susakovich@gmail.com>
2023-08-01 22:09:48 +0200

    Finish 8kyu kata

---
+++ 8kyu/basicVariableAssingment/main.js
@@ -0,0 +1,13 @@
+// TODO
+/*
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+let a == "code";
+let b == "wa.rs";
+let name == a + b;
+*/
+
+let a = "code";
+let b = "wa.rs";
+let name = a + b;
+console.log(name);
