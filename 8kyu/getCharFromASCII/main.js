6300f4131ae9767ad4b3faed7bd078e2e755b97e
Danijel <susakovich@gmail.com>
2023-08-04 08:31:28 +0200

    Finish 8kyu kata

---
+++ 8kyu/getCharFromASCII/main.js
@@ -0,0 +1,12 @@
+// TODO
+/*
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+
+*/
+const getChar = String.fromCharCode;
