54f024e0a71b3d2cab560afa35bea6b0428b5a85
Danijel <susakovich@gmail.com>
2023-08-14 09:32:43 +0200

    Finish 8kyu kata

---
+++ 8kyu/printArrWithComaDelimeter/main.js
@@ -0,0 +1,11 @@
+// TODO
+/*
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+*/
+const printArray = (array) => array.join(",");
