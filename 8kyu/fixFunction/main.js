e1284825cc230d272291ddb6924f9f371c4bbbd4
Danijel <susakovich@gmail.com>
2023-08-18 08:51:47 +0200

    Finish 8kyu kata

---
+++ 8kyu/fixFunction/main.js
@@ -0,0 +1,17 @@
+// TODO
+/*
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+
+function addFive(num) {
+  var total = num + 5
+  return num
+}
+*/
+
+function addFive(num) {
+  return num + 5;
+}
+
+console.log(addFive(5));
