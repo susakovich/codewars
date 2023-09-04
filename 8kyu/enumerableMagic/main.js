83614e1f3cd4dae473790cfd87f91fb38bfa503a
Danijel <susakovich@gmail.com>
2023-09-04 07:20:51 +0200

    Finish 8kyu kata

---
+++ 8kyu/enumerableMagic/main.js
@@ -0,0 +1,5 @@
+// TODO
+/*
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+const include = (arr, item) => arr.includes(item);
