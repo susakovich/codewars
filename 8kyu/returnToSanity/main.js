f7e40e5cb6a4350c22cc9d897e950191ca5423a9
Danijel <susakovich@gmail.com>
2023-09-23 00:32:43 +0200

    Finish 8kyu kata

---
+++ 8kyu/returnToSanity/main.js
@@ -0,0 +1,16 @@
+// TODO
+/*
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+function mystery() {
+  let results =
+    {sanity: 'Hello'};
+  return
+    results;
+}
+*/
+
+function mystery() {
+  let results = { sanity: "Hello" };
+  return results;
+}
