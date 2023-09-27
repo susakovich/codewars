911040492d6d3e46e566928710ffc14b4e537555
Danijel <susakovich@gmail.com>
2023-09-28 00:33:25 +0200

    Finish 8kyu kata

---
+++ 8kyu/addItemToArr/main.js
@@ -0,0 +1,13 @@
+// TODO
+/*
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+let websites = [];
+*/
+
+let websites = [];
+
+websites.push("codewars");
