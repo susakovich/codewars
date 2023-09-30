a4422da82f4513869ccf94e14469cf9f3e94182d
Danijel <susakovich@gmail.com>
2023-10-01 00:14:10 +0200

    Finish 8kyu kata

---
+++ 8kyu/regexVerification/main.js
@@ -0,0 +1,13 @@
+// TODO
+/*
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+*/
+
+function validateUsr(username) {
+  return /^[0-9a-z_]{4,16}$/.test(username);
+}
