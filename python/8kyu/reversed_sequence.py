5192534a47b8208c2ba2d93ea9a222b30b4af4da
Danijel <susakovich@gmail.com>
2023-11-02 16:33:18 +0100

    Finish 8kyu kata

---
+++ python/8kyu/reversed_sequence.py
@@ -0,0 +1,9 @@
+"""
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+"""
+
+
+def reverse_seq(n):
+    return [i for i in range(n, 0, -1)]
