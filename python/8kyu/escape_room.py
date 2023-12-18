3f00903c4a3a704930e4bcc015446accb08f01a8
Danijel <susakovich@gmail.com>
2023-12-18 20:39:02 +0100

    Finish 8kyu kata

---
+++ python/8kyu/escape_room.py
@@ -0,0 +1,21 @@
+"""
+Escape the room
+You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+"""
+rooms = {
+    "room1": {
+        "name": "Living Room",
+        "description": "A cozy living room with a fireplace.",
+        "completed": False,
+    },
+    "room2": {
+        "name": "Kitchen",
+        "description": "A modern kitchen with stainless steel appliances.",
+        "completed": False,
+    },
+    "room3": {
+        "name": "Bedroom",
+        "description": "A comfortable bedroom with a large bed.",
+        "completed": False,
+    },
+}
