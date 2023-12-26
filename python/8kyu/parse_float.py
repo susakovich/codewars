90083d8d8b1f6d27cc1b962cb4fbf1891553bb60
Danijel <susakovich@gmail.com>
2023-12-26 21:03:15 +0100

    Finish 8kyu kata

---
+++ python/8kyu/parse_float.py
@@ -0,0 +1,10 @@
+"""
+Write function parse_float which takes a string/list and returns a number or 'none' if conversion is not possible.
+"""
+
+
+def parse_float(string):
+    try:
+        return float(string)
+    except:
+        return None
