4608f5592c8ce43fcfcb9151bff7084ca57d488a
Danijel <susakovich@gmail.com>
2024-01-05 21:43:47 +0100

    Finish 8kyu kata

---
+++ python/8kyu/quarter_of_year.py
@@ -0,0 +1,23 @@
+"""
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+Constraint:
+
+1 <= month <= 12
+"""
+
+
+def quarter_of(month):
+    if 1 <= month <= 3:
+        return 1
+    elif 4 <= month <= 6:
+        return 2
+    elif 7 <= month <= 9:
+        return 3
+    elif 10 <= month <= 12:
+        return 4
+    else:
+        # Handle invalid month input
+        return None
