45611d7fa86b55a18c91c83edcf3d45ed918458e
Danijel <susakovich@gmail.com>
2024-02-04 20:08:06 +0100

    Finish 7kyu kata

---
+++ python/7kyu/row_weights.py
@@ -0,0 +1,12 @@
+"""
+Scenario
+Several people are standing in a row divided into two teams.
+The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+Task
+Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+"""
+
+
+def row_weights(array):
+    return sum(array[::2]), sum(array[1::2])
