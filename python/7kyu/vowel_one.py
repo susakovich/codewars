b921ccc7296f4472fd20c374d9ed07f5e296bfef
Danijel <susakovich@gmail.com>
2024-03-04 19:54:36 +0100

    Finish 7kyu kata

---
+++ python/7kyu/vowel_one.py
@@ -0,0 +1,16 @@
+"""
+vowelOne
+Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+
+All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+
+Examples:
+
+vowelOne "abceios" -- "1001110"
+
+vowelOne "aeiou, abc" -- "1111100100"
+"""
+
+
+def vowel_one(s):
+    return "".join("1" if c in "aeiou" else "0" for c in s.lower())
