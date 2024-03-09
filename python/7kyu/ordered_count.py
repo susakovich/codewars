a79b402f461b8ad6aa776c41bfd0cb3f664cc370
Danijel <susakovich@gmail.com>
2024-03-09 20:33:21 +0100

    Finish 7kyu kata

---
+++ python/7kyu/ordered_count.py
@@ -0,0 +1,15 @@
+"""
+Count the number of occurrences of each character and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
+
+Consult the solution set-up for the exact data structure implementation depending on your language.
+
+Example:
+
+ordered_count("abracadabra") == [('a', 5), ('b', 2), ('r', 2), ('c', 1), ('d', 1)]
+"""
+
+from collections import Counter
+
+
+def ordered_count(input):
+    return list(Counter(input).items())
