7d5e4d7f91d9d00ddbd2f5243e31ca67005a496d
Danijel <susakovich@gmail.com>
2024-03-10 09:53:47 +0100

    Finish 7kyu kata

---
+++ python/7kyu/get_names.py
@@ -0,0 +1,27 @@
+"""
+The following code is not giving the expected results. Can you debug what the issue is?
+
+The following is an example of data that would be passed in to the function.
+
+data = [
+    {'name': 'Joe', 'age': 20},
+    {'name': 'Bill', 'age': 30},
+    {'name': 'Kate', 'age': 23}
+]
+get_names(data) # should return ['Joe', 'Bill', 'Kate']
+
+
+def itemgetter(item):
+    item['name']
+    
+def get_names(data):
+    return list(map(itemgetter, data))
+"""
+
+
+def itemgetter(item):
+    return item["name"]
+
+
+def get_names(data):
+    return list(map(itemgetter, data))
